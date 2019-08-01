01d82c52521f2c4fa2ed62a3d7b76ec6edfbbac4
Unknown <d.long271293@gmail.com>
2019-08-02 01:36:17 +0700

    apply RESTFUL

--- passengerfeedback/passenger_client/main.go
+++ passengerfeedback/passenger_client/main.go
@@ -4,8 +4,10 @@ import (
 	pb "../proto"
 	"context"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"google.golang.org/grpc"
 	"log"
+	"net/http"
 	"strconv"
 	"strings"
 	"time"
@@ -17,7 +19,7 @@ const (
 
 var client pb.FeedbackClient
 
-var methods = map[int]string {
+var methods = map[int]string{
 	1: "Add Feedback",
 	2: "Get Feedback By PassengerID",
 	3: "Get Feedback By BookingCode",
@@ -25,174 +27,207 @@ var methods = map[int]string {
 	5: "Stop the program",
 }
 
-func addAndPrintPassengerFeedback(bookingCode string, passengerId int32, feedback string, printMsg bool) {
-	// Contact the server and print out its response.
+func handleAddFeedback(c *gin.Context) {
+	var argument struct {
+		PassengerId string
+		BookingCode string
+		Feedback    string
+	}
+	err := c.BindJSON(&argument)
+	if err != nil {
+		c.String(400, "invalid param \n")
+		log.Print(err)
+		return
+	}
+	if len(strings.TrimSpace(argument.PassengerId)) == 0 {
+		c.JSON(http.StatusOK, gin.H{"errorCode": pb.Error_FAIL, "message": "Missing passengerId"})
+		return
+	}
+	passengerId, err := strconv.Atoi(argument.PassengerId)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"errorCode": pb.Error_FAIL, "message": "PassengerId must be a number"})
+		return
+	}
+	if len(strings.TrimSpace(argument.BookingCode)) == 0 {
+		c.JSON(http.StatusOK, gin.H{"errorCode": pb.Error_FAIL, "message": "Missing bookingCode"})
+		return
+	}
+	if len(strings.TrimSpace(argument.Feedback)) == 0 {
+		c.JSON(http.StatusOK, gin.H{"errorCode": pb.Error_FAIL, "message": "Missing feedback"})
+		return
+	}
+	res, err := addPassengerFeedback(argument.BookingCode, int32(passengerId), argument.Feedback)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"errorCode": pb.Error_FAIL, "message": "Something happens"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"errorCode": pb.Error_SUCCESS, "message": res.Msg, "data": res.Data})
+}
+
+func handleGetFeedbackByPassengerId(c *gin.Context) {
+	strPassenger, b := c.GetQuery("passengerId")
+	if !b || len(strings.TrimSpace(strPassenger)) == 0 {
+		c.JSON(http.StatusOK, gin.H{"errorCode": pb.Error_FAIL, "message": "Missing passengerId"})
+		return
+	}
+	passengerId, err := strconv.Atoi(strPassenger)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"errorCode": pb.Error_FAIL, "message": "PassengerId must be a number"})
+		return
+	}
+	strOffset, b := c.GetQuery("offset")
+	if !b || len(strings.TrimSpace(strOffset)) == 0 {
+		c.JSON(http.StatusOK, gin.H{"errorCode": pb.Error_FAIL, "message": "Missing Offset"})
+		return
+	}
+	offset, err := strconv.Atoi(strOffset)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"errorCode": pb.Error_FAIL, "message": "Offset must be a number"})
+		return
+	}
+	strLimit, b := c.GetQuery("limit")
+	if !b || len(strings.TrimSpace(strLimit)) == 0 {
+		c.JSON(http.StatusOK, gin.H{"errorCode": pb.Error_FAIL, "message": "Missing Limit"})
+		return
+	}
+	limit, err := strconv.Atoi(strLimit)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"errorCode": pb.Error_FAIL, "message": "Limit must be a number"})
+		return
+	}
+
+	response, err := getFeedbackByPassengerId(int32(passengerId), int32(offset), int32(limit))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"errorCode": pb.Error_FAIL, "message": err.Error()})
+		log.Print(err)
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"errorCode": response.ErrorCode, "message": response.Msg, "data": response.Data})
+}
+
+func handleGetFeedbackByBookingCode(c *gin.Context) {
+	strBookingCode, b := c.GetQuery("bookingCode")
+	if !b || len(strings.TrimSpace(strBookingCode)) == 0 {
+		c.JSON(http.StatusOK, gin.H{"errorCode": pb.Error_FAIL, "message": "Missing bookingCode"})
+		return
+	}
+	strOffset, b := c.GetQuery("offset")
+	if !b || len(strings.TrimSpace(strOffset)) == 0 {
+		c.JSON(http.StatusOK, gin.H{"errorCode": pb.Error_FAIL, "message": "Missing Offset"})
+		return
+	}
+	offset, err := strconv.Atoi(strOffset)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"errorCode": pb.Error_FAIL, "message": "Offset must be a number"})
+		return
+	}
+	strLimit, b := c.GetQuery("limit")
+	if !b || len(strings.TrimSpace(strLimit)) == 0 {
+		c.JSON(http.StatusOK, gin.H{"errorCode": pb.Error_FAIL, "message": "Missing Limit"})
+		return
+	}
+	limit, err := strconv.Atoi(strLimit)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"errorCode": pb.Error_FAIL, "message": "Limit must be a number"})
+		return
+	}
+
+	response, err := getFeedbackByBookingCode(strBookingCode, int32(offset), int32(limit))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"errorCode": pb.Error_FAIL, "message": err.Error()})
+		log.Print(err)
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"errorCode": response.ErrorCode, "message": response.Msg, "data": response.Data})
+}
+
+func handleDeleteFeedbackByPassengerId(c *gin.Context) {
+	var argument struct {
+		PassengerId string
+	}
+	err := c.BindJSON(&argument)
+	if err != nil {
+		c.String(400, "invalid param \n")
+		log.Print(err)
+		return
+	}
+	if len(strings.TrimSpace(argument.PassengerId)) == 0 {
+		c.JSON(http.StatusOK, gin.H{"errorCode": pb.Error_FAIL, "message": "Missing passengerId"})
+		return
+	}
+	passengerId, err := strconv.Atoi(argument.PassengerId)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"errorCode": pb.Error_FAIL, "message": "PassengerId must be a number"})
+		return
+	}
+
+	response, err := deleteFeedbackByPassengerId(int32(passengerId))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"errorCode": pb.Error_FAIL, "message": err.Error()})
+		log.Print(err)
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"errorCode": response.ErrorCode, "message": response.Msg})
+}
+
+func addPassengerFeedback(bookingCode string, passengerId int32, feedback string) (res *pb.PassengerFeedbackResponse, err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
-	r, err := client.AddPassengerFeedback(ctx, &pb.PassengerFeedbackRequest{
+	res, err = client.AddPassengerFeedback(ctx, &pb.PassengerFeedbackRequest{
 		BookingCode: bookingCode,
 		PassengerId: passengerId,
 		Feedback:    feedback,
 	})
-
-	if err != nil {
-		fmt.Println("could not add passenger:", err)
-		return
-	}
-
-	if printMsg {
-		fmt.Println(r.Msg)
-	}
-
-	if r.ErrorCode == pb.Error_SUCCESS {
-		fmt.Println(r.Data.String())
-	}
-
+	return
 }
 
-func generateSomeFeedback(out chan<- bool, number int) {
-	for i := 1; i <= number; i++ {
-		addAndPrintPassengerFeedback("DRIVE0" + strconv.Itoa(i), 1, "Good service.", false)
-	}
-	out <- true
-}
-
-func addPassengerFromInput() {
-	var passengerId int32
-	var bookingCode string
-	var feedback string
-
-	fmt.Print("Input Passenger Id: ")
-	_, err := fmt.Scan(&passengerId)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	fmt.Print("Input Booking Code: ")
-	_, err = fmt.Scan(&bookingCode)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	if len(strings.TrimSpace(bookingCode)) == 0 {
-		fmt.Println("Booking code expected at least one character.")
-		return
-	}
-	fmt.Print("Input feedback: ")
-	_, err = fmt.Scan(&feedback)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	if len(strings.TrimSpace(feedback)) == 0 {
-		fmt.Println("Feedback expected at least one character.")
-		return
-	}
-	addAndPrintPassengerFeedback(bookingCode, passengerId, feedback, true)
-}
-
-func getFeedbackByPassengerIdFromInput() {
-	var passengerId int32
-	var offset int32
-	var limit int32
-	fmt.Print("Input Passenger Id: ")
-	_, err := fmt.Scan(&passengerId)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	fmt.Print("Input offset: ")
-	_, err = fmt.Scan(&offset)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	fmt.Print("Input limit: ")
-	_, err = fmt.Scan(&limit)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
+func getFeedbackByPassengerId(passengerId, offset, limit int32) (*pb.PassengerFeedbackSliceResponse, error) {
 	// Contact the server and print out its response.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
 	r, err := client.GetFeedbackByPassengerId(ctx, &pb.GetPassengerFeedbackByPassengerIdRequest{
 		PassengerId: passengerId,
-		Offset: offset,
-		Limit: limit,
+		Offset:      offset,
+		Limit:       limit,
 	})
 
 	if err != nil {
-		fmt.Println("could not get feedbacks by passenger:", err)
-		return
+		return nil, err
 	}
 
-	fmt.Println(r.Msg)
-	if r.ErrorCode == pb.Error_SUCCESS && len(r.Data) > 0 {
-		for _, v := range r.Data {
-			fmt.Println(v)
-		}
-	}
+	return r, nil
 }
 
-func getFeedbackByBookingCodeFromInput() {
-	var bookingCode string
-	fmt.Print("Input Booking Code: ")
-
-	_, err := fmt.Scan(&bookingCode)
-
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	if len(strings.TrimSpace(bookingCode)) == 0 {
-		fmt.Println("Booking code expected at least one character.")
-		return
-	}
-
+func getFeedbackByBookingCode(bookingCode string, offset, limit int32) (*pb.PassengerFeedbackSliceResponse, error) {
 	// Contact the server and print out its response.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
 	r, err := client.GetFeedbackByBookingCode(ctx, &pb.PassengerFeedbackByBookingCodeRequest{
 		BookingCode: bookingCode,
+		Offset:      offset,
+		Limit:       limit,
 	})
 
 	if err != nil {
-		fmt.Println("could not get feedbacks by booking code:", err)
-		return
+		return nil, err
 	}
 
-	fmt.Println(r.Msg)
-	if r.ErrorCode == pb.Error_SUCCESS {
-		fmt.Println(r.Data.String())
-	}
-
+	return r, nil
 }
 
-func deleteFeedbackByPassengerIdFromInput() {
-	var passengerId int32
-
-	fmt.Print("Input Passenger Id: ")
-
-	_, err := fmt.Scan(&passengerId)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
+func deleteFeedbackByPassengerId(passengerId int32)  (*pb.ErrorCodeAndMessageResponse, error)  {
 	// Contact the server and print out its response.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
-	r, err := client.DeleteFeedbackByPassengerId(ctx, &pb.DeletePassengerFeedbackByPassengerIdRequest{PassengerId:passengerId})
+	r, err := client.DeleteFeedbackByPassengerId(ctx, &pb.DeletePassengerFeedbackByPassengerIdRequest{PassengerId: passengerId})
 	if err != nil {
-		fmt.Println("could not delete feedback by passenger:", err)
-		return
+		return nil, err
 	}
 
-	fmt.Println(r.Msg)
+	return r, nil
 }
 
 func main() {
@@ -204,55 +239,19 @@ func main() {
 	defer conn.Close()
 	client = pb.NewFeedbackClient(conn)
 
-	fmt.Println("Before you choose a method, I'll add some passenger feedbacks to the local variable for passenger Id 1 " +
-		"with booking from DRIVE01 to DRIVE010. ")
-	number := 10
+	router := gin.Default()
 
-	chanInitFeedback := make(chan bool)
+	router.POST("/addFeedback", handleAddFeedback)
+	//localhost:8080/addFeedback
+	//body raw: {"bookingCode": "DRIVE01", "passengerId": "1", "feedback": "Good"}
+	router.GET("/getFeedbackByPassengerId", handleGetFeedbackByPassengerId)
+	//localhost:8080/getFeedbackByPassengerId?passengerId=1&offset=0&limit=10
+	router.GET("/getFeedbackByBookingCode", handleGetFeedbackByBookingCode)
+	//localhost:8080/getFeedbackByBookingCode?bookingCode=DRIVE02&offset=0&limit=10
+	router.DELETE("/deleteFeedback", handleDeleteFeedbackByPassengerId)
+	//localhost:8080/deleteFeedback
+	//body raw: {"passengerId":"2"}
+	router.Run()
 
-	go generateSomeFeedback(chanInitFeedback, number)
 
-	<-chanInitFeedback
-
-	fmt.Println("DONE")
-	fmt.Println("Now is your turn. Following the instruction: ")
-
-	for {
-		fmt.Println("----Choose Method----")
-		for i := 1; i <= len(methods); i++ {
-			fmt.Println(i, ":", methods[i])
-		}
-
-		fmt.Print("> ")
-		var methodID int
-		_, err := fmt.Scan(&methodID)
-		if err != nil {
-			log.Fatal(err)
-			continue
-		}
-		if methodID < 1 || methodID > len(methods) {
-			fmt.Println("Invalid Method")
-			continue
-		}
-
-		fmt.Println("You choose:", methods[methodID])
-
-		switch methodID {
-		case 1:
-			addPassengerFromInput()
-			continue
-		case 2:
-			getFeedbackByPassengerIdFromInput()
-			continue
-		case 3:
-			getFeedbackByBookingCodeFromInput()
-			continue
-		case 4:
-			deleteFeedbackByPassengerIdFromInput()
-			continue
-		case 5:
-			fmt.Println("The program stops")
-			return
-		}
-	}
 }
--- passengerfeedback/passenger_server/main.go
+++ passengerfeedback/passenger_server/main.go
@@ -13,52 +13,57 @@ const (
 	port = ":50052"
 )
 
-var feedbacks = map[string]pb.PassengerFeedback{}
+var feedbacks = map[int32][]pb.PassengerFeedback{}
 
 type server struct{}
 
 func (s *server) AddPassengerFeedback(ctx context.Context, in *pb.PassengerFeedbackRequest) (*pb.PassengerFeedbackResponse, error) {
-	if _, ok := feedbacks[in.BookingCode]; !ok {
-		pfb := pb.PassengerFeedback{
-			BookingCode: in.BookingCode,
-			PassengerId: in.PassengerId,
-			Feedback:    in.Feedback,
+	if feedbackByPassenger, ok := feedbacks[in.PassengerId]; ok {
+		for _, fb := range feedbackByPassenger {
+			if fb.BookingCode == in.BookingCode {
+				return &pb.PassengerFeedbackResponse{
+					Msg:       "Feedback exists",
+					ErrorCode: pb.Error_FAIL,
+				}, nil
+			}
 		}
-		feedbacks[in.BookingCode] = pfb
-		return &pb.PassengerFeedbackResponse{
-			Data:      &pfb,
-			Msg:       "Thank you for your feedback",
-			ErrorCode: pb.Error_SUCCESS,
-		}, nil
 	}
+
+	pfb := pb.PassengerFeedback{
+		BookingCode: in.BookingCode,
+		PassengerId: in.PassengerId,
+		Feedback:    in.Feedback,
+	}
+	feedbacks[in.PassengerId] = append(feedbacks[in.PassengerId], pfb)
 	return &pb.PassengerFeedbackResponse{
-		Msg:       "Feedback exists with the bookingCode",
-		ErrorCode: pb.Error_FAIL,
+		Data:      &pfb,
+		Msg:       "Thank you for your feedback",
+		ErrorCode: pb.Error_SUCCESS,
 	}, nil
 }
 
 func (s *server) GetFeedbackByPassengerId(ctx context.Context, in *pb.GetPassengerFeedbackByPassengerIdRequest) (out *pb.PassengerFeedbackSliceResponse, err error) {
 	out = new(pb.PassengerFeedbackSliceResponse)
 	if len(feedbacks) > 0 {
-		var sliceFeedbacks []string
-		for k, v := range feedbacks {
-			if v.PassengerId == in.PassengerId {
-				sliceFeedbacks = append(sliceFeedbacks, k)
-			}
-		}
+		var sliceFeedbacks []int32
+		passengerFeedbacks := feedbacks[in.PassengerId]
 
-		if dataLength := len(sliceFeedbacks); dataLength < int(in.Offset) {
-			sliceFeedbacks = []string{}
-		} else if dataLength < int(in.Offset+in.Limit) {
-			sliceFeedbacks = sliceFeedbacks[in.Offset:]
+		if dataLength := int32(len(passengerFeedbacks)); dataLength < in.Offset {
+			sliceFeedbacks = []int32{}
+		} else if dataLength < in.Offset+in.Limit {
+			for i := in.Offset; i < dataLength; i++ {
+				sliceFeedbacks = append(sliceFeedbacks, i)
+			}
 		} else {
-			sliceFeedbacks = sliceFeedbacks[in.Offset:in.Limit]
+			for i := in.Offset; i < in.Limit; i++ {
+				sliceFeedbacks = append(sliceFeedbacks, i)
+			}
 		}
 
 		if num := len(sliceFeedbacks); num > 0 {
 			out.Msg = "The passenger has " + strconv.Itoa(num) + " feedbacks"
 			for i := 0; i < num ; i++ {
-				v := feedbacks[sliceFeedbacks[i]]
+				v := feedbacks[in.PassengerId][i]
 				out.Data = append(out.Data, &v)
 			}
 
@@ -73,17 +78,41 @@ func (s *server) GetFeedbackByPassengerId(ctx context.Context, in *pb.GetPasseng
 	return out, nil
 }
 
-func (s *server) GetFeedbackByBookingCode(ctx context.Context, in *pb.PassengerFeedbackByBookingCodeRequest) (*pb.PassengerFeedbackResponse, error) {
-	if feedback, ok := feedbacks[in.BookingCode]; ok {
-		return &pb.PassengerFeedbackResponse{Data: &feedback, Msg: "Success!", ErrorCode: pb.Error_SUCCESS}, nil
+func (s *server) GetFeedbackByBookingCode(ctx context.Context, in *pb.PassengerFeedbackByBookingCodeRequest) (*pb.PassengerFeedbackSliceResponse, error) {
+	out := new(pb.PassengerFeedbackSliceResponse)
+	if len(feedbacks) > 0 {
+		for _, feedbackByPassenger := range feedbacks {
+			for k, fb := range feedbackByPassenger {
+				if fb.BookingCode == in.BookingCode {
+					out.Data = append(out.Data, &feedbackByPassenger[k])
+				}
+			}
+		}
+		if dataLength := int32(len(out.Data)); dataLength < in.Offset {
+			out.Data = out.Data[0:0]
+		} else if dataLength < in.Offset+in.Limit {
+			out.Data = out.Data[in.Offset:]
+		} else {
+			out.Data = out.Data[in.Offset:in.Limit]
+		}
+
+		if num := len(out.Data); num > 0 {
+			out.Msg = "The booking code has " + strconv.Itoa(num) + " feedbacks"
+
+		} else {
+			out.Msg = "The booking code has no feedback"
+		}
+
+		return out, nil
 	}
-	return &pb.PassengerFeedbackResponse{Msg: "There is no feedback at booking code " + in.BookingCode, ErrorCode: pb.Error_FAIL}, nil
+	out.Msg = "The booking code has no feedback"
+	return out, nil
 }
 
 func (s *server) DeleteFeedbackByPassengerId(ctx context.Context, in *pb.DeletePassengerFeedbackByPassengerIdRequest) (*pb.ErrorCodeAndMessageResponse, error) {
-	for k,v := range feedbacks {
-		if v.PassengerId == in.PassengerId {
-			delete(feedbacks, k)
+	if len(feedbacks) > 0 {
+		if _, ok := feedbacks[in.PassengerId]; ok {
+			delete(feedbacks, in.PassengerId)
 		}
 	}
 	return &pb.ErrorCodeAndMessageResponse{Msg: "Success"}, nil
